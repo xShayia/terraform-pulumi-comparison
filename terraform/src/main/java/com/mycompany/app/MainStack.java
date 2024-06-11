29fcb41e73b9328258435cd180b2966ecd0622a2
alexandra <alexandramargeanu@gmail.com>
2024-06-12 01:01:19 +0300

    created subnet,vpc,cidr,database

--- terraform/src/main/java/com/mycompany/app/MainStack.java
+++ terraform/src/main/java/com/mycompany/app/MainStack.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.hashicorp.cdktf.Token;
 import com.hashicorp.cdktf.providers.aws.apprunner_service.ApprunnerService;
@@ -18,6 +20,7 @@ import com.hashicorp.cdktf.providers.aws.cloudfront_distribution.CloudfrontDistr
 import com.hashicorp.cdktf.providers.aws.cloudfront_distribution.CloudfrontDistributionViewerCertificate;
 import com.hashicorp.cdktf.providers.aws.cloudfront_origin_access_control.CloudfrontOriginAccessControl;
 import com.hashicorp.cdktf.providers.aws.cloudfront_origin_access_control.CloudfrontOriginAccessControlConfig;
+import com.hashicorp.cdktf.providers.aws.data_aws_db_instance.DataAwsDbInstance;
 import com.hashicorp.cdktf.providers.aws.data_aws_ecr_authorization_token.DataAwsEcrAuthorizationToken;
 import com.hashicorp.cdktf.providers.aws.data_aws_ecr_authorization_token.DataAwsEcrAuthorizationTokenConfig;
 import com.hashicorp.cdktf.providers.aws.data_aws_iam_policy_document.DataAwsIamPolicyDocument;
@@ -25,16 +28,31 @@ import com.hashicorp.cdktf.providers.aws.data_aws_iam_policy_document.DataAwsIam
 import com.hashicorp.cdktf.providers.aws.data_aws_iam_policy_document.DataAwsIamPolicyDocumentStatement;
 import com.hashicorp.cdktf.providers.aws.data_aws_iam_policy_document.DataAwsIamPolicyDocumentStatementCondition;
 import com.hashicorp.cdktf.providers.aws.data_aws_iam_policy_document.DataAwsIamPolicyDocumentStatementPrincipals;
+import com.hashicorp.cdktf.providers.aws.db_instance.DbInstance;
+import com.hashicorp.cdktf.providers.aws.db_instance.DbInstanceConfig;
+import com.hashicorp.cdktf.providers.aws.db_subnet_group.DbSubnetGroup;
+import com.hashicorp.cdktf.providers.aws.db_subnet_group.DbSubnetGroupConfig;
 import com.hashicorp.cdktf.providers.aws.ecr_repository.EcrRepository;
 import com.hashicorp.cdktf.providers.aws.ecr_repository.EcrRepositoryConfig;
 import com.hashicorp.cdktf.providers.aws.iam_policy_attachment.IamPolicyAttachment;
 import com.hashicorp.cdktf.providers.aws.iam_role.IamRole;
 import com.hashicorp.cdktf.providers.aws.iam_role.IamRoleConfig;
+import com.hashicorp.cdktf.providers.aws.internet_gateway.InternetGateway;
+import com.hashicorp.cdktf.providers.aws.internet_gateway.InternetGatewayConfig;
 import com.hashicorp.cdktf.providers.aws.provider.AwsProvider;
+import com.hashicorp.cdktf.providers.aws.route_table.RouteTable;
+import com.hashicorp.cdktf.providers.aws.route_table.RouteTableConfig;
+import com.hashicorp.cdktf.providers.aws.route_table.RouteTableRoute;
+import com.hashicorp.cdktf.providers.aws.route_table_association.RouteTableAssociation;
+import com.hashicorp.cdktf.providers.aws.route_table_association.RouteTableAssociationConfig;
 import com.hashicorp.cdktf.providers.aws.s3_bucket.S3Bucket;
 import com.hashicorp.cdktf.providers.aws.s3_bucket.S3BucketConfig;
 import com.hashicorp.cdktf.providers.aws.s3_bucket_policy.S3BucketPolicy;
 import com.hashicorp.cdktf.providers.aws.s3_bucket_policy.S3BucketPolicyConfig;
+import com.hashicorp.cdktf.providers.aws.subnet.Subnet;
+import com.hashicorp.cdktf.providers.aws.subnet.SubnetConfig;
+import com.hashicorp.cdktf.providers.aws.vpc.Vpc;
+import com.hashicorp.cdktf.providers.aws.vpc.VpcConfig;
 import com.hashicorp.cdktf.providers.docker.image.Image;
 import com.hashicorp.cdktf.providers.docker.image.ImageBuild;
 import com.hashicorp.cdktf.providers.docker.image.ImageConfig;
@@ -159,8 +177,6 @@ public class MainStack extends TerraformStack {
 		);
 
 
-
-
 		IamRole iamRole = new IamRole(this, "iamRole", IamRoleConfig.builder()
 				.assumeRolePolicy(Token.asString(appRunIamRole.getJson()))
 				.managedPolicyArns(List.of("arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"))
@@ -182,6 +198,61 @@ public class MainStack extends TerraformStack {
 						.build()
 				)
 				.build());
+
+
+		//availability zones
+		List<String> azs = List.of("eu-central-1a", "eu-central-1b", "eu-central-1c");
+
+
+		Vpc vpc = new Vpc(this, "vpc", VpcConfig.builder()
+				.cidrBlock("10.0.0.0/16")
+				.build());
+
+		List<String> public_subnet_cidrs = List.of("10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24");
+		List<String> private_subnet_cidrs = List.of("10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24");
+
+
+		//ip distribution to azs
+		var cidr_to_azs_public = Map.of(public_subnet_cidrs.get(0), azs.get(0), public_subnet_cidrs.get(1), azs.get(1), public_subnet_cidrs.get(2), azs.get(2));
+		var cidr_to_azs_private = Map.of(private_subnet_cidrs.get(0), azs.get(0), private_subnet_cidrs.get(1), azs.get(1), private_subnet_cidrs.get(2), azs.get(2));
+
+
+		Map<String, Subnet> publicSubnetMap = new HashMap<>();
+
+
+		public_subnet_cidrs
+				.forEach(cidr -> {
+					publicSubnetMap.put(cidr, new Subnet(this, "_" + cidr.replace(".", "_")
+							.replace("/", "_"), SubnetConfig.builder()
+							.cidrBlock(cidr).vpcId(vpc.getId()).availabilityZone(cidr_to_azs_public.get(cidr))
+							.build()));
+				});
+		var private_subnet = private_subnet_cidrs.stream()
+				.map(cidr -> new Subnet(this, "_" + cidr.replace(".", "_").replace("/", "_"), SubnetConfig.builder()
+						.vpcId(vpc.getId()).cidrBlock(cidr).availabilityZone(cidr_to_azs_private.get(cidr)).build()))
+				.toList();
+
+
+		//conexiune vpc si internet (subnet sa poata accesa internetul)
+		InternetGateway internetGateway = new InternetGateway(this, "internetGateway", InternetGatewayConfig.builder()
+				.vpcId(vpc.getId()).build());
+
+		RouteTable routeTable = new RouteTable(this, "routeTable", RouteTableConfig.builder().vpcId(vpc.getId())
+				.route(List.of(RouteTableRoute.builder().cidrBlock("0.0.0.0/0").gatewayId(internetGateway.getId())
+						.build())).build());
+		public_subnet_cidrs.forEach(cidr -> new RouteTableAssociation(this, "_" + "_" + cidr.replace(".", "_")
+				.replace("/", "_"), RouteTableAssociationConfig.builder().subnetId(publicSubnetMap.get(cidr).getId())
+				.routeTableId(routeTable.getId()).build()));
+
+
+		DbSubnetGroup dbSubnetGroup = new DbSubnetGroup(this, "dbSunetGroup", DbSubnetGroupConfig.builder().name("main")
+				.subnetIds(private_subnet.stream().map(Subnet::getId).toList()).build());
+
+
+		DbInstance dbInstance = new DbInstance(this, "dbInstance", DbInstanceConfig.builder().allocatedStorage(10)
+				.dbName("mydb").engine("mysql").engineVersion("8.0").instanceClass("db.t3.micro").username("user")
+				.password("useruser").parameterGroupName("default.mysql8.0").skipFinalSnapshot(true)
+				.dbSubnetGroupName(dbSubnetGroup.getName()).build());
 
 
 //        Container.Builder.create(this, "nginxContainer")
